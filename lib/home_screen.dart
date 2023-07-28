6e8b601fc2c431da2d761a0e2f416395f0532815
azix-khan <azixkhan.55@gmail.com>
2023-07-28 22:32:47 +0500

    threads an instagram app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
